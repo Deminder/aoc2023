bd011b2437efbb526590b35666bf5291bbf7fecd
Deminder <tremminder@gmail.com>
2023-12-02 13:18:50 +0100

    day1: Simplify solution

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,45 +1,23 @@
-use regex::Regex;
-
 use crate::PuzzleInput;
 
-fn label_to_num(num_label: &str) -> u32 {
-    if let Ok(num) = num_label.parse() {
-        num
-    } else {
-        [
-            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        ]
-        .into_iter()
-        .enumerate()
-        .find(|(_, label)| *label == num_label)
-        .map(|(i, _)| i as u32 + 1)
-        .unwrap()
-    }
-}
-
-fn line_to_lables<'a>(line: &'a str, label_regex: &Regex) -> Vec<&'a str> {
-    let mut start = 0;
-    let mut labels = vec![];
-    while let Some(m) = label_regex.find_at(line, start) {
-        start = m.start() + 1;
-        labels.push(m.as_str());
-    }
-    labels
+fn labelstart_to_num(label_start: &str, part2: bool) -> Option<u32> {
+    [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .into_iter()
+    .position(|label| part2 && label_start.starts_with(label))
+    .map(|pos| pos as u32 + 1)
+    .or_else(|| label_start.chars().next().and_then(|c| c.to_digit(10)))
 }
 
 fn run(input: PuzzleInput, part2: bool) -> u32 {
-    let digit_regex = Regex::new(if part2 {
-        r"\d|one|two|three|four|five|six|seven|eight|nine"
-    } else {
-        r"\d"
-    })
-    .unwrap();
     input
         .map(|line| {
-            let labels = line_to_lables(&line, &digit_regex);
-            let (first, last) = if let Some(f) = labels.first() {
-                let first_num = label_to_num(f);
-                (first_num, labels.last().map_or(first_num, |l| label_to_num(l)))
+            let mut nums = line
+                .char_indices()
+                .filter_map(|(i, _)| labelstart_to_num(&line[i..], part2));
+            let (first, last) = if let Some(first_num) = nums.next() {
+                (first_num, nums.last().unwrap_or(first_num))
             } else {
                 (0, 0)
             };
@@ -58,10 +36,17 @@ mod test {
 
     #[test]
     fn test_run() {
-        assert_eq!(run(r"1abc2
+        assert_eq!(
+            run(
+                r"1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet".into(), false), 142);
+treb7uchet"
+                    .into(),
+                false
+            ),
+            142
+        );
         assert_eq!(run("soneighttwo39ktl132".into(), true), 12);
         assert_eq!(run("oneight".into(), true), 18);
         assert_eq!(run("oneightwo".into(), true), 12);
@@ -73,7 +58,8 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen".into(),
+7pqrstsixteen"
+                    .into(),
                 true
             ),
             281

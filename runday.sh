1e0166368487e5ed9cacf789662d7c45ee502998
Deminder <tremminder@gmail.com>
2023-12-06 12:42:12 +0100

    runday: Fetch puzzle input with curl

--- runday.sh
+++ runday.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 day=${1:?"Missing day argument"}
+INPUT_FILE="src/day${day}/input.txt"
+SESSION_KEY_FILE=session.txt
+if [ ! -f "$INPUT_FILE" ] && [ -f "$SESSION_KEY_FILE" ]
+then
+	curl "https://adventofcode.com/2023/day/$day/input" \
+		--compressed \
+		-A "curl/$(curl --version | head -n 1 | cut -f2-3 -d' ') ($(cargo read-manifest | jq -r '.homepage,.authors[0]' | tr '\n' ' '))" \
+		-H "Cookie: session=$(cat "$SESSION_KEY_FILE")" \
+		--create-dirs \
+		--output "$INPUT_FILE"
+fi
 for part in 1 2
 do
-	cargo run -rq -- -i src/day${day}/input.txt ${day} ${part}
+	cargo run -rq -- -i "$INPUT_FILE" ${day} ${part}
 done
